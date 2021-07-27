b8ff271789d763e7cb0310b700f8e60ad5154f9c
Jocelyn Lecamus <>
2021-07-28 00:15:53 +0900

    add param in example

--- demo/index.jsx
+++ demo/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Footer, Logo, SocialBar, TutorialTitle } from '../src/main'
+import { CONFIG_BRANDING, Footer, Logo, SocialBar, TutorialTitle } from '../src/main'
 
 ReactDOM.render(
   <React.StrictMode>
-    <TutorialTitle number={'01'} />
-    <SocialBar />
-    <Footer />
+    <TutorialTitle number={"01"} theme={CONFIG_BRANDING.THEME_DARK} />
+    <SocialBar theme={CONFIG_BRANDING.THEME_DARK} />
+    <Footer theme={CONFIG_BRANDING.THEME_DARK} />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
